f647651917248bd85fbd5fee56e9143e169992fe
Allen Hsu <hsu.y.allen@gmail.com>
2017-03-08 11:10:32 -0500

    Initialization of Final Project

---
+++ app/src/main/java/allenhsu/cst2335_final_project/MainActivity.java
@@ -0,0 +1,13 @@
+package allenhsu.cst2335_final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
